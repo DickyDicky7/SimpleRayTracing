5f35a786cb6d3f9264829bc1e6775d8fd32d63ce
pham_tuan_anh <pham_tuan_anh@optimizer.co.jp>
2025-03-31 11:05:01 +0700

    add other allocators ~03

---
+++ FreelistAllocator.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef FREELIST_ALLOCATOR_H
+#define FREELIST_ALLOCATOR_H
+
+
+
+  #include <cstddef>
+//#include <cstddef>
+  #include <stdexcept>
+//#include <stdexcept>
+  #include <new> // For placement new
+//#include <new> // For placement new
+  #include <utility> // For std::forward
+//#include <utility> // For std::forward
+
+
+
+class FreelistAllocator
+{
+};
+
+
+
+#endif
