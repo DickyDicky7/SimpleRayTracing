12ecc975dacaa77868531cfa1df3eada6ab8cb11
pham_tuan_anh <pham_tuan_anh@optimizer.co.jp>
2025-03-31 11:04:55 +0700

    add other allocators ~02

---
+++ PoolAllocator.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef POOL_ALLOCATOR_H
+#define POOL_ALLOCATOR_H
+
+
+
+  #include <cstddef>
+//#include <cstddef>
+  #include <stdexcept>
+//#include <stdexcept>
+  #include <new> // For placement new
+//#include <new> // For placement new
+  #include <utility> // For std::forward
+//#include <utility> // For std::forward
+
+
+
+class PoolAllocator
+{
+};
+
+
+
+#endif
